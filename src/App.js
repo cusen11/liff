3bd9d8f4e2a908bb9cd3aad051b5cf4b31f48bc5
cusen11 <you@example.com>
2023-06-15 16:00:01 +0700

    sd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DowloadPage from './Pages/DowloadPage';
-// import HomePage from './Pages/HomePage';
-import HomePage1 from './Pages/HomePage1';
+import HomePage from './Pages/HomePage';
+// import HomePage1 from './Pages/HomePage1';
 
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
   return (
     <BrowserRouter>   
         <Routes>  
-          {/* <Route path="/" element={<HomePage/>}/>   */}
-          <Route path="/" element={<HomePage1/>}/>  
+          <Route path="/" element={<HomePage/>}/>  
+          {/* <Route path="/" element={<HomePage1/>}/>   */}
           <Route path="/dowload" element={<DowloadPage/>}/>  
         </Routes>  
   </BrowserRouter> 
